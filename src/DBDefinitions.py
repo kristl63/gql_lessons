1910f66685d69aa0b60a725de766936a059b18d0
Hrbolek <alexandr.stefek@unob.cz>
2024-09-03 17:28:21 +0200

    enabling any connection string

--- src/DBDefinitions.py
+++ src/DBDefinitions.py
@@ -173,5 +173,6 @@ def ComposeConnectionString():
 
     driver = "postgresql+asyncpg"  # "postgresql+psycopg2"
     connectionstring = f"{driver}://{user}:{password}@{hostWithPort}/{database}"
+    connectionstring = os.environ.get("CONNECTION_STRING", connectionstring)
 
     return connectionstring
