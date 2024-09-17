89529b597896a719e37ecaf6977e6837eb5ea7d3
Hrbolek <alexandr.stefek@unob.cz>
2024-09-17 21:47:52 +0200

    AcSemesterGQLModel, AcTopicGQLModel explicitly into schema

--- src/GraphTypeDefinitions/__init__.py
+++ src/GraphTypeDefinitions/__init__.py
@@ -85,5 +85,6 @@ class Mutation:
 from .UserGQLModel import UserGQLModel
 from .EventGQLModel import EventGQLModel
 from .FacilityGQLModel import FacilityGQLModel
-
-schema = strawberry.federation.Schema(query=Query, mutation=Mutation, types=(UserGQLModel, EventGQLModel, FacilityGQLModel))
+from .AcSemesterGQLModel import AcSemesterGQLModel
+from .AcTopicGQLModel import AcTopicGQLModel
+schema = strawberry.federation.Schema(query=Query, mutation=Mutation, types=(UserGQLModel, EventGQLModel, FacilityGQLModel, AcSemesterGQLModel, AcTopicGQLModel))
